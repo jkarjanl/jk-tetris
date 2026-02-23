8bc57df5356b988b5b6e93bc8732be686123533f
Janne Karjanlahti <janne@karjanlahti.fi>
2026-02-23 22:58:50 +0200

    First SDL window

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 CC	:= gcc
 CFLAGS	:= -std=c99 -Wall -Wextra -Wpedantic -Werror -O2
-LDFLAGS	:=
+CFLAGS  += $(shell pkg-config --cflags sdl2)
+
+LDFLAGS := $(shell pkg-config --libs sdl2)
 
 TARGET	:= jk-tetris
 SRC	:= src/main.c
--- src/main.c
+++ src/main.c
@@ -1,6 +1,42 @@
+#include <SDL2/SDL.h>
 #include <stdio.h>
 
 int main(void) {
-    puts("Hello World!");
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        "jk-tetris",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        800, 600,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (!window) {
+        fprintf(stderr, "SDL_CreateWindow failed: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // Pieni event loop: ikkuna pysyy auki kunnes suljet sen
+    int running = 1;
+    while (running) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+
+        // Tässä kohtaa myöhemmin piirrät ruudun (renderer tms.)
+        SDL_Delay(16); // ~60fps "hengähdys"
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
+
